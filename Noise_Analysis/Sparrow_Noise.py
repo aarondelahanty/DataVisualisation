38518753d9fe0dbc7280a2be3f01217bbcc46cd6
Aaron <aarondelahanty@gmail.com>
2020-04-15 15:42:16 +0200

    Update Sparrow_Noise.py
    updated code for ledgability

--- Noise_Analysis/Sparrow_Noise.py
+++ Noise_Analysis/Sparrow_Noise.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 import pandas as pd
 
@@ -8,17 +9,18 @@ data = pd.read_csv("E5_Analysis.csv")
 df = pd.DataFrame(data)
 
 #extract each feautre as patial dataframe
+#Generate blank array for inserting temperary column
+blank = np.zeros(15)
+
+#Break out data into amplitude, maximum, height (peak to peak), and minimum datsframes
+
 amp = df.iloc[:,::4]
-df.insert(1,"temp",[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
+df.insert(1,"temp",blank,True)
 max = df.iloc[:,::4]
-df.insert(1,"temp",[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
+df.insert(1,"temp",blank,True)
 height = df.iloc[:,::4]
-df.insert(1,"temp",[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
+df.insert(1,"temp",blank,True)
 min = df.iloc[:,::4]
-
-
-# Print data frame column labels aka axes
-#print(data.axes)
 
 # print time average
 print(amp.mean())
