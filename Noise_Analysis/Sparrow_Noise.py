c09647da4c7bf91b5adf1411a45c5c4d1d67d990
aarondelahanty <aarondelahanty@users.noreply.github.com>
2020-04-16 17:55:23 +0200

    Updated plots with x-axis labels
    amplitude, min, max, height all plotted suceesfully, still needs y-axis tweaking.

--- Noise_Analysis/Sparrow_Noise.py
+++ Noise_Analysis/Sparrow_Noise.py
@@ -9,19 +9,26 @@ data = pd.read_csv("E5_Analysis.csv")
 df = pd.DataFrame(data)
 
 
-#Break out data into amplitude, maximum, height (peak to peak), and minimum datsframes
+#Break out data into amplitude, maximum, height (peak to peak), and minimum dataframes
 
 amp = df.iloc[:,::4]
-amp_avg = amp.mean()
-amp_avg = amp_avg.transpose()
+amp_avg = pd.DataFrame(amp.mean())
+amp_avg = amp_avg.melt()
+amp_avg.columns = ['electrode','voltage']
+print(amp_avg['electrode'])
+amp_avg['electrode'] = np.arange(1,62)
 # df.insert(1,"temp",blank,True)
 max = df.iloc[:,1::4]
-max_avg = max.mean()
-max_avg = max_avg.transpose()
+max_avg = pd.DataFrame(max.mean())
+max_avg = max_avg.melt()
+max_avg.columns = ['electrode','voltage']
+max_avg['electrode'] = np.arange(1,61)
 # df.insert(1,"temp",blank,True)
 height = df.iloc[:,2::4]
-height_avg = height.mean()
-height_avg = height_avg.transpose()
+height_avg = pd.DataFrame(height.mean())
+height_avg = height_avg.melt()
+height_avg.columns = ['electrode','voltage']
+height_avg['electrode'] = np.arange(1,61)
 # df.insert(1,"temp",blank,True)
 min = df.iloc[:,3::4]
 min_avg = pd.DataFrame(min.mean())
@@ -29,23 +36,23 @@ min_avg = min_avg.melt()
 min_avg.columns = ['electrode','voltage']
 min_avg['electrode'] = np.arange(1,61)
 # print each subdataframe
-print(amp_avg)
-print(max_avg)
-print(height_avg)
-print(min_avg)
+# print(amp_avg)
+# print(max_avg)
+# print(height_avg)
+# print(min_avg)
 
 f, axes = plt.subplots(2,2)
 
-g=sns.swarmplot(x = None, y=amp_avg, ax=axes[0,0], palette='pastel',)
-# g.tick_params('x',labelrotation=60)
+g=sns.swarmplot(x=amp_avg['electrode'], y=amp_avg['voltage'], ax=axes[0,0], palette='pastel',)
+g.set_title('amplitude')
 
-h=sns.swarmplot(x = None, y=max.mean(),ax=axes[0,1], palette='pastel')
-# h.tick_params('x',labelrotation=60)
+h=sns.swarmplot(x=max_avg['electrode'], y=max_avg['voltage'], ax=axes[0,1], palette='pastel')
+h.set_title('maximum')
+
+i=sns.swarmplot(x=min_avg['electrode'], y=min_avg['voltage'], ax=axes[1,0], palette='pastel')
+i.set_title('minimum')
 #
-i=sns.swarmplot(x=[""]*len(min_avg), y=min_avg['voltage'],ax=axes[1,0], palette='Blues', hue = min_avg['electrode'])
-# i.tick_params('x',labelrotation=60)
-#
-j=sns.swarmplot(x = None, y=height.mean(),ax=axes[1,1], palette='pastel')
-# j.tick_params('x',labelrotation=60)
+j=sns.swarmplot(x=height_avg['electrode'], y=height_avg['voltage'], ax=axes[1,1], palette='pastel')
+j.set_title('height')
 
 plt.show()
